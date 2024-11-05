87a9c347257d71a3dac5f66c93eead7236197789
Renato <rverissimo.dev@gmail.com>
2024-11-05 12:23:54 -0300

    Implementing a max priority queue

---
+++ greedy/priority-queue-binary-heap.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int H[50];
+int size = -1;
+
+
+
+int parent(int i){
+	return (i - 1) / 2;
+}
+
+int left_child(int i){
+	return ((2 * i) + 1);
+}
+
+int right_child(int i){
+	return ((2 * i) + 2);
+}
+
+void shiftUp(int i){
+	
+	while(i > 0 && H[parent(i)] < H[i]){
+		swap(H[parent(i)], H[i]);
+		i = parent(i);
+	}
+	
+}
+
+void shiftDown(int i){
+	
+	int max_index = i;
+	
+	int l = left_child(i);
+	
+	if(l <= size && H[l] > H[max_index]) max_index = l;
+	
+	int r = right_child(i);
+	
+	if(r <= size && H[r] > H[max_index]) max_index = r;
+	
+	if(i != max_index){
+		swap(H[i], H[max_index]);
+		shiftDown(max_index);
+	}
+}
+
+void insert(int p){
+	size = size + 1;
+	H[size] = p;
+	shiftUp(size);
+}
+
+int extractMax(){
+	int result = H[0];
+	H[0] = H[size];
+	size = size - 1;
+	shiftDown(0);
+	return result;
+}
+
+int getMax(){
+	return H[0];
+}
+
+void remove(int i){
+	
+}
+
+
+int main(){
+	
+	
+	return 0;
+}
