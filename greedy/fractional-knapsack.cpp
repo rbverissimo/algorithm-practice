095b5dff20f56d6def8a68e5037fbce7227cfb66
Renato <rverissimo.dev@gmail.com>
2024-11-18 13:54:44 -0300

    Full implementation of the Fractional Knapsack algorithm

--- greedy/fractional-knapsack.cpp
+++ greedy/fractional-knapsack.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+using namespace std;
 
 
 struct Item {
@@ -19,15 +20,33 @@ static bool comp(struct Item a, struct Item b){
 
 double fractKpsk(int capacity, struct Item arr[], int N){
 	
+	sort(arr, arr + N, comp);
+	double rst = 0.0;
 	
-	return 0.0;
+	
+	for(int i = 0; i < N; i++){
+		
+		if(arr[i].weight <= capacity){
+			capacity -= arr[i].weight;
+			rst += arr[i].profit;	
+		} else {
+			rst += arr[i].profit * ((double) capacity / (double) arr[i].weight); 
+			break;
+		}	
+	}
+	
+	return rst;
 	
 }
 
 int main(){
 	
-	int capacity = 50;
+	int capacity = 60;
 	Item arr[] = { {30, 5}, {20, 10}, {100, 20}, {90, 30}, {160, 40} };
+	
+	int N = sizeof(arr) / sizeof(arr[0]);
+	
+	cout << fractKpsk(capacity, arr, N);
 	
 	return 0;
 }
