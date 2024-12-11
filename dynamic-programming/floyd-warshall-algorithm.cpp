16ba744e5e4be064ccc6d62e8c03d15a3ec79714
Renato <rverissimo.dev@gmail.com>
2024-12-11 17:16:24 -0300

    Understanding the Floyd Warshall Algorithm

---
+++ dynamic-programming/floyd-warshall-algorithm.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+
+#define V 4
+#define INF 9999
+
+
+
+void floydWarshall(int dist[][V]){
+	
+}
+
+int main(){
+	
+	
+	int graph[V][V] = {
+		{
+			0, 5, INF, 10
+		},
+		{
+			INF, 0, 3, INF	
+		},
+		{
+			INF, INF, 0, 1
+		},
+		{
+			INF, INF, INF, 0
+		}
+	
+	}; 
+	
+	
+	return 0;
+	
+}
