78fbf4f27770d6719bb49fd0e4ae8d2c27739fe1
Renato <rverissimo.dev@gmail.com>
2024-12-26 10:47:12 -0300

    understanding a little better dynamic programming and the rod cutting problem (using DP and limiting the cut in half)

--- dynamic-programming/rod-cutting-dp-approach.cpp
+++ dynamic-programming/rod-cutting-dp-approach.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+//EXPLAIN: the ans variable of the rodRecurisve is the return of the evalutation of the current answer and the price between the the beginning - rest of the array
+// meaning: if we cut the array at index 2, then the array should be cut at index 6. 
+
 
 int cutRodRecursive(int i, vector<int> &price){
 	
@@ -14,16 +17,18 @@ int cutRodRecursive(int i, vector<int> &price){
 	return ans;
 }
 
-//0, 0,
 
 int cutRod(vector<int> &price){
 	
-	int n = price.size();
-	return cutRodRecursive(n, price);
+	int n = price.size();  
+	return cutRodRecursive(n, price); 
 }
 
 int main(){
 	
+	vector<int> price = { 1, 5, 8, 9, 10, 17, 17, 20 };
+	
+	cout << cutRod(price);
 	
 	return 0;
 }
