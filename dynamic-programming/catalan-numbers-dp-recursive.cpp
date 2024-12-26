81d423f82ed58ab46cff8d1e9e192eb1861e3dfb
Renato <rverissimo.dev@gmail.com>
2024-12-26 11:20:37 -0300

    refactoring catalan numbers code for conciseness

--- dynamic-programming/catalan-numbers-dp-recursive.cpp
+++ dynamic-programming/catalan-numbers-dp-recursive.cpp
@@ -6,9 +6,7 @@ int findCatalan(int n){
 	
 	if(n < 2) return 1;
 	int res = 0;
-	for(int i = 0; i < n; i++){
-		res += findCatalan(i) * findCatalan(n - i - 1);
-	} 
+	for(int i = 0; i < n; i++) res += findCatalan(i) * findCatalan(n - i - 1);
 	return res;
 	
 }
@@ -16,7 +14,6 @@ int findCatalan(int n){
 int main(){
 	
 	int n = 10;
-	
 	for(int i = 0; i < n + 1; i++) cout << i << ": " << findCatalan(i) << "\n";
 	
 	return 0;
