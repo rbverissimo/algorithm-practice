cf212ca5ce8ad5323a308fcb405bde40e104db35
Renato <rverissimo.dev@gmail.com>
2024-12-26 11:19:49 -0300

    Understanding catalan numbers

---
+++ dynamic-programming/catalan-numbers-dp-recursive.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int findCatalan(int n){
+	
+	if(n < 2) return 1;
+	int res = 0;
+	for(int i = 0; i < n; i++){
+		res += findCatalan(i) * findCatalan(n - i - 1);
+	} 
+	return res;
+	
+}
+
+int main(){
+	
+	int n = 10;
+	
+	for(int i = 0; i < n + 1; i++) cout << i << ": " << findCatalan(i) << "\n";
+	
+	return 0;
+}
