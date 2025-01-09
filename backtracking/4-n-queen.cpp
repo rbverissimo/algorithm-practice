42f8c40004af8596bbe185f047d0154afbc31a14
Renato <rverissimo.dev@gmail.com>
2025-01-09 17:27:18 -0300

    Understanding the N Queen backtracking algorithm

---
+++ backtracking/4-n-queen.cpp
@@ -0,0 +1,31 @@
+#define N 4
+#include<stdio.h>
+#include<stdbool.h>
+
+
+bool solveNQUtil(int board[N][N], int col){
+	
+	if(col >= N) return true;
+
+	return false;	
+}
+
+
+bool solveNQ(){
+	
+	int board[N][N] = {
+		{0, 0, 0, 0,},
+		{0, 0, 0, 0,},
+		{0, 0, 0, 0,},
+		{0, 0, 0, 0,}
+	};
+	
+	return true;
+	
+}
+
+int main(){
+	
+	solveNQ();
+	return 0;
+}
