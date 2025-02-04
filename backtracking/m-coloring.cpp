95a55f0fbd06b94ee9cd5dec8d7227c9310d4eff
Renato <rverissimo.dev@gmail.com>
2025-02-04 12:36:47 -0300

    Fully understood the backtracking approach to the m-coloring problem. Beautiful solution

--- backtracking/m-coloring.cpp
+++ backtracking/m-coloring.cpp
@@ -5,16 +5,67 @@
 // m-coloring using backtracking technique
 
 
+void printSolution(int color[]);
+
+
+/*
+	if vertex v is linked to another vertex (== 1)
+	AND the color has already been taken by given vertex, 
+	THEN it isn't safe to place a color in the array
+*/	
+bool isSafe(int v, bool graph[V][V], int color[], int c){
+	for(int i = 0; i < V; i++)
+		if(graph[v][i] && c == color[i]) return false;
+	return true;	
+}
+
+bool graphColoringUtil(bool graph[V][V], int m, int color[], int v){
 	
-bool isSafe(int v, int graph[V][V], int color[], int c){
+	
+	if(v == V) return true;
+	
+	for(int c = 1; c <= m; c++){
+		if(isSafe(v, graph, color, c)){
+			color[v] = c;
+			
+			if(graphColoringUtil(graph, m, color, v+1)) return true;
+			
+			color[v] = 0;
+		}
+	}
+	
+	//return false to backtrack the solution
+	return false;
+	
+}
+
+bool graphColoring(bool graph[V][V], int m){
+	
+	int color[V];
+	for(int i = 0; i < V; i++) color[i] = 0;
+	
+	if(!graphColoringUtil(graph, m, color, 0)){
+		printf("Solution doesn't exist \n'");
+		return false;
+	}
+	
+	printSolution(color);
 	return true;
+	
+}
+
+void printSolution(int color[]){
+	printf("Solution: ");
+	for(int i = 0; i < V; i++)
+		printf(" %d ", color[i]);
+	printf("\n");
 }
 
 int main(){
 	
 	
 	//adjacency matrix
-	int graph[V][V] = {
+	bool graph[V][V] = {
 		{
 			0, 1, 1, 1
 		},
@@ -31,6 +82,6 @@ int main(){
 	
 	int m = 3; 
 	
-	
+	graphColoring(graph, m);
 	return 0;
 }
