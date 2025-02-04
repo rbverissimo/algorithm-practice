8c05bcf6fffe70ec438c3f053acf7730678d6842
Renato <rverissimo.dev@gmail.com>
2025-02-04 11:51:59 -0300

    starting to understand the m-coloring problem

---
+++ backtracking/m-coloring.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+#define V 4
+// m-coloring using backtracking technique
+
+
+	
+bool isSafe(int v, int graph[V][V], int color[], int c){
+	return true;
+}
+
+int main(){
+	
+	
+	//adjacency matrix
+	int graph[V][V] = {
+		{
+			0, 1, 1, 1
+		},
+		{
+			1, 0, 1, 1
+		},
+		{
+			1, 1, 0, 1
+		},
+		{
+			1, 0, 1, 0
+		}
+	};
+	
+	int m = 3; 
+	
+	
+	return 0;
+}
