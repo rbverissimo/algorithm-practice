de88a29ae2cbbfa27bb3519c141681eb6b0107b9
Renato <rverissimo.dev@gmail.com>
2025-02-12 17:14:04 -0300

    Understanding a bit more of the solution using the branch and bound technique

--- branch&bound/0-1-knapsack.cpp
+++ branch&bound/0-1-knapsack.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <algorithm>
+#include <queue>
 
 //Implementing the 0/1 knapsack using B&B
 
@@ -24,13 +25,35 @@ int bound(Node u, int n, int W, Item arr[]){
 	if(u.weight >= W) return 0;
 	
 	int profit_bound = u.bound;
+	int j = u.level + 1;
+	int totweight = u.weight;
+	
+	while((j < n) && (totweight + arr[j].weight < W)){
+		totweight += arr[j].weight;
+		profit_bound = arr[j].value;
+		j++;
+	}
+	
+	if(j < n) profit_bound += (W - totweight) * arr[j].value / arr[j].weight;
 	
 	
+	return profit_bound;
+}
+
+int knapsack(int W, Item arr[], int n){
 	return 1;
 }
 
 
 int main(){
+	
+	int W = 10;
+	
+	Item arr[] = {
+		{2, 40},{3.14, 50},{1.98, 100},{5, 95},{3, 30}
+	};
+	
+	int n = sizeof(arr)/sizeof(arr[0]);
 	
 	return 0;
 }
